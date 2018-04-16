105efd525efc28535b1f94298ec5a4231d686075
WeiFoo <wfu@ncsu.edu>
2018-04-16 00:19:43 -0400

    HW6

---
+++ HWs/Week 6/S6_MergeTwoSortedLists.py
@@ -0,0 +1,54 @@
+"""
+Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
+
+Example:
+
+Input: 1->2->4, 1->3->4
+Output: 1->1->2->3->4->4
+"""
+
+
+from helpers import Node, getLinkedList, createLinkedList
+
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+def mergeTwoLists(l1, l2):
+		"""
+	  l1: ListNode
+	  l2: ListNode
+	  """
+		### Please start your code here###
+
+		### End ###
+
+
+"""
+
+Below are the test cases I created for testing the correctness of your code.
+Please don't modify them when you push the file back to GitHub
+"""
+
+if __name__ == '__main__':
+        test0 = ([], [], [])
+        test1 = ([1], [1], [1, 1])
+        test2 = ([1,2], [3,4,5,6], [1,2,3,4,5,6])
+        test3 = ([1,2,3], [1,2,3], [1,1,2,2,3,3,])
+        tests = [test0, test1, test2, test3]
+        for i, test in enumerate(tests):
+                print "------------- Test " +str(i) + " -------------"
+                print "-Test Input:"
+                print test[0]
+                print "-Expected Output:"
+                print test[1]
+                print "-Your Output:"
+                head1 = createLinkedList(test[0])
+                head2 = createLinkedList(test[1])
+                your_head = mergeTwoLists(head1, head2)
+                your_ans = getLinkedList(your_head)
+                print your_ans
+                assert your_ans == test[2], "Wrong return. Please try again."
+        print '\n**** Congratulations! You have passed all the tests! ****'
+        
---
+++ HWs/Week 6/S6_SearchInsertPosition.py
@@ -0,0 +1,61 @@
+"""
+Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You may assume no duplicates in the array.
+
+Example 1:
+
+Input: [1,3,5,6], 5
+Output: 2
+
+Example 2:
+
+Input: [1,3,5,6], 2
+Output: 1
+
+Example 3:
+
+Input: [1,3,5,6], 7
+Output: 4
+
+Example 4:
+
+Input: [1,3,5,6], 0
+Output: 0
+
+"""
+
+def searchInsert(nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: int
+    """
+    ### Please start your code here###
+
+		### End ###
+
+"""
+
+Below are the test cases I created for testing the correctness of your code.
+Please don't modify them when you push the file back to GitHub
+"""
+
+if __name__ == '__main__':
+        test0 = ([], 1, 0)
+        test1 = ([1,3,5,6], 5, 2)
+        test2 = ([1,3,5,6], 2, 1)
+        test3 = ([1,3,5,6], 7, 4)
+        test4 = ([1,3,5,6], 0, 0)
+        tests = [test0, test1, test2, test3, test4]
+        for i, test in enumerate(tests):
+                print "------------- Test " +str(i) + " -------------"
+                print "-Test Input:"
+                print test[0]
+                print "-Expected Output:"
+                print test[1]
+                print "-Your Output:"
+                your_ans = searchInsert(test[0], test[1])
+                print your_ans
+                assert your_ans == test[2], "Wrong return. Please try again."
+        print '\n**** Congratulations! You have passed all the tests! ****'
